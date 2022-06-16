eda58d9acac145e24ceacbe2be59f6e719d02030
Astzesk <107450114+Astzesk@users.noreply.github.com>
2022-06-17 00:32:37 +0100

    Add the message.py file with the tagDict and reportDict.

---
+++ project/data/message.py
@@ -0,0 +1,32 @@
+tagDict = {
+    "null": "",
+    "msg": "\nMessage: ",
+}
+
+reportDict = {
+    "null": [
+        "",
+    ],
+    "program": [
+        "The program closed!\n",
+        "Try another option!",
+    ],
+    "decision": [
+        "Yes",
+        "No",
+    ],
+    "invoice" : [
+        "Payment has been successfully!",
+        "Payment has been unsuccessfully!",
+        "\nCoin(s) and banknote(s) received:\n",
+    ],
+    "error": [
+        "\nUnexpected error: ",
+        "The value is incompatible!",
+    ],
+    "request": [
+        "\nInsert a price value: ",
+        "Insert a payment value: ",
+        "\nDo you want close program (Yes or No)?\n",
+    ],
+}
