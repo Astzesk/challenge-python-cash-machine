a497471fee4efcdcd68f4df62dae9bc61ea981c5
Astzesk <107450114+Astzesk@users.noreply.github.com>
2022-07-15 23:38:24 +0100

    Add a new constructor and a destructor to the Init class.

--- project/app/resources/init.py
+++ project/app/resources/init.py
@@ -2,8 +2,14 @@ from .modules.cash_machine.include.bank import *
 
 class Init(Bank):
 
+    def __init__(self, lPrice = 0, lPayment = 0):
+        super().__init_subclass__(lPrice, lPayment)
+
     def __init_subclass__(cls, lPrice = 0, lPayment = 0):
         super().__init_subclass__(lPrice, lPayment)
 
     def __del__(self) -> None:
+        super().__del__()
+
+    def __del__(cls) -> None:
         super().__del__()
